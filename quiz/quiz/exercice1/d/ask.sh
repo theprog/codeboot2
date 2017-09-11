5b53cd92b091364f73d2dae1d955838302de0ef3
Ide Codeboot <codeboot@arcade03.iro.umontreal.ca>
2017-09-11 19:23:23 -0400

    Quiz: fix some questions

--- quiz/quiz/exercice1/d/ask.sh
+++ quiz/quiz/exercice1/d/ask.sh
@@ -5,7 +5,7 @@ name="$1"
 total=$(wc -l < questions)
 
 q=$(echo "$name" | sha1sum | sed -r 's/[a-z]//g' | awk '{print $1}')
-n=$(echo "$q % $total" | bc)
+n=$(echo "$q % ($total - 1) + 1" | bc)
 
 hexa=$(sed "${n}q;d" questions)
 
--- quiz/quiz/exercice1/d/correct.sh
+++ quiz/quiz/exercice1/d/correct.sh
@@ -6,11 +6,9 @@ input=$(echo "$2" | tr '[:upper:]' '[:lower:]')
 total=$(wc -l < questions)
 
 q=$(echo "$name" | sha1sum | sed -r 's/[a-z]//g' | awk '{print $1}')
-n=$(echo "$q % $total" | bc)
+n=$(echo "$q % ($total - 1) + 1" | bc)
 
 hexa=$(sed "${n}q;d" questions)
-
-decimal=$(echo 'console.log('$hexa')' | node)
 
 ! test "$hexa" = "$input"
 echo $?
