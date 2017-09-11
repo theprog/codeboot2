96bba5e9bd7f24a75bedd47b13bc11df9de6e861
Nicolas Hurtubise <316k@legtux.org>
2017-09-11 19:02:02 -0400

    Quiz: use a bash script to correct exercice1/e

--- quiz/quiz/exercice1/e/correct.sh
+++ quiz/quiz/exercice1/e/correct.sh
@@ -3,10 +3,33 @@
 name="$1"
 input="$2"
 
-parsed=$(echo "$input" | sed -r 's/\+/ /')
+parsed=$(echo "$input" | sed -r 's/[^0-9.+]//g')
 
-a=$(echo $parsed | awk '{print $1}')
-b=$(echo $parsed | awk '{print $2}')
+numbers=$(echo "$input" | sed -r 's/\+/ /')
+
+a=$(echo $numbers | awk '{print $1}')
+b=$(echo $numbers | awk '{print $2}')
 
 
-./check "$a" "$b"
+if ! [ $(echo "$a" | bc) = $(echo 'console.log('"$a"')' | node) ]
+then
+    echo 0
+    exit
+fi
+
+if ! [ $(echo "$b" | bc) = $(echo 'console.log('"$b"')' | node) ]
+then
+    echo 0
+    exit
+fi
+
+eval_bc=$(echo "$parsed" | bc)
+eval_node=$(echo 'console.log('"$parsed"')' | node)
+
+if [ "$eval_bc" = "$eval_node" ]
+then
+    echo 0
+    exit
+fi
+
+echo "$a" '> 1.0 && ' "$a" '< 10.0 && ' "$b" '> 1.0 && ' "$b" '< 10.0' | bc
